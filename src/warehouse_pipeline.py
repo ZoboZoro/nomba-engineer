23f2fe45d6ed1a01dcabd923f91601fb91e02457
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-11-22 16:17:47 +0100

    pipeline

--- src/warehouse_pipeline.py
+++ src/warehouse_pipeline.py
@@ -1,16 +1,13 @@
 import logging
-import time
-import psycopg2
-import pandas as pd
-from sqlalchemy import create_engine
-from datetime import datetime, timedelta
-from pymongo import MongoClient
-import subprocess
-import boto3
+from datetime import datetime
+
 import awswrangler as wr
+import pandas as pd
+from pymongo import MongoClient
+from sqlalchemy import create_engine
 
-
-from utils.config import DBNAME, WH_HOST, HOST, WH_LOG_FILE, USER, PASS, WH_PORT, PORT, DB, DATA_DIR, CONNECTION_STRING
+from utils.config import (CONNECTION_STRING, DBNAME, HOST, PASS, PORT, USER,
+                          WH_LOG_FILE)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -20,12 +17,23 @@ logging.basicConfig(
     )
 
 
-postgres_src_engine = create_engine(f"postgresql+psycopg2://{USER}:{PASS}@{HOST}:{PORT}/{DBNAME}")
+postgres_src_engine = create_engine(
+     f"postgresql+psycopg2://{USER}:{PASS}@{HOST}:{PORT}/{DBNAME}"
+     )
+BUCKET = "nomba-dumpss"
+time_stamp = datetime.now().strftime("%Y-%m-%d,%H-%M-%S")
 
-def from_mongodb():
+
+def mongodb_tos3() -> None:
     client = MongoClient(CONNECTION_STRING)
     collection = client[DBNAME].get_collection("users")
-    return pd.DataFrame(collection.find({}))
+    users_data = pd.DataFrame(collection.find({}))
+    wr.s3.to_csv(
+                df=users_data,
+                path=f's3://{BUCKET}/source_data_dumps/{time_stamp}_users.csv',
+                dataset=False,
+                )
+    logging.info(f"Written {len(users_data)} records to staging layer!")
 
 
 # mongo_data_file = f"user_collections{time_stamp}.csv"
@@ -41,8 +49,8 @@ def from_mongodb():
 #     ], capture_output=True)
 
 
-def from_postgres_source():
-    #connect to source
+def postgresSource_tos3():
+    # connect to source
     src_table1 = pd.read_sql_table(
         table_name="savings_plan",
         schema="bronze",
@@ -54,28 +62,27 @@ def from_postgres_source():
         schema="bronze",
         con=postgres_src_engine,
     )
-    
-    return src_table1, src_table2
 
+    tables = {
+        "savings_plan": src_table1,
+        "savings_transaction": src_table2
+        }
 
-tables = {
-    "savings_plan": from_postgres_source()[0],
-    "savings_transaction": from_postgres_source()[1],
-    "users": from_mongodb()
-    }
+    # Load to staging layer
+    for table, table_data in tables.items():
+        wr.s3.to_csv(
+                df=table_data,
+                path=f"s3://{BUCKET}/source_data_dumps/\
+                {time_stamp}{table}.csv",
+                dataset=False,
+                )
+        logging.info(
+                 f"Written {len(table_data)} records from\
+                 {table} to staging layer!"
+                 )
 
-BUCKET = "nomba-dumpss"
-time_stamp = datetime.now().strftime("%Y-%m-%d,%H-%M-%S")
 
 #  Writing to storage staging layer
 if __name__ == "__main__":
-    try:
-        for table, table_data in tables.items():
-            wr.s3.to_csv(
-                df=table_data,
-                path=f's3://{BUCKET}/source_data_dumps/{time_stamp}{table}.csv',
-                dataset=False,
-                )
-            logging.info(f"Written {len(table_data)} records from {table} to staging layer!")
-    except Exception as e:
-        logging.error(e)
+    mongodb_tos3()
+    postgresSource_tos3()
