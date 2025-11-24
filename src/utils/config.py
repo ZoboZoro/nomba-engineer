41a3b5dc0927281d0e7399fd0795fa4df5bae437
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-11-24 19:22:49 +0100

    config

--- src/utils/config.py
+++ src/utils/config.py
@@ -10,8 +10,8 @@ dotenv_path = f"{BASE_DIR}/.env"
 load_dotenv(dotenv_path, verbose=True)
 
 # MongoDB config
-MONGO_USER = os.getenv('MONGO_USER')
-MONGO_PASS = os.getenv('MONGO_PASS')
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
 CONNECTION_STRING = f"mongodb://{MONGO_USER}:{MONGO_PASS}@localhost:27017/"
 client = MongoClient(CONNECTION_STRING)
 DB = client["nomba"]
@@ -23,17 +23,17 @@ SQL = SQL_DIR.mkdir(exist_ok=True)
 SQL_FILE = BASE_DIR / "src" / "sql" / "query.sql"
 LOG_DIR = BASE_DIR / "logs"
 LOG = LOG_DIR.mkdir(exist_ok=True)
-PG_LOG_FILE = LOG_DIR/"postgres_src.log"
-WH_LOG_FILE = LOG_DIR/"warehouse_pipeline.log"
-MG_LOG_FILE = LOG_DIR/"mongo_src.log"
+PG_LOG_FILE = LOG_DIR / "postgres_src.log"
+WH_LOG_FILE = LOG_DIR / "warehouse_pipeline.log"
+MG_LOG_FILE = LOG_DIR / "mongo_src.log"
 DATA_DIR = BASE_DIR / "data"
 DATA = BASE_DIR.mkdir(exist_ok=True)
 
 # SQL_DB config
-USER = os.getenv('PG_USER')
-PASS = os.getenv('PG_PASS')
-HOST = os.getenv('PG_HOST', "postgres_src")
-WH_HOST = os.getenv('PG_HOST', "postgres_des")
-DBNAME = os.getenv('DB_NAME')
-PORT = int(os.getenv('SRC_PG_PORT', 5432))
-WH_PORT = int(os.getenv('DES_PG_PORT', 5432))
+USER = os.getenv("PG_USER")
+PASS = os.getenv("PG_PASS")
+HOST = os.getenv("PG_HOST", "postgres_src")
+WH_HOST = os.getenv("PG_HOST", "postgres_des")
+DBNAME = os.getenv("DB_NAME")
+PORT = int(os.getenv("SRC_PG_PORT", 5432))
+WH_PORT = int(os.getenv("DES_PG_PORT", 5432))
