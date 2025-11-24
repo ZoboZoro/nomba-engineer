e3f5e43bd042fa7e156453f2c9c1bc888e45ca39
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-11-24 19:23:31 +0100

    pipeline

--- src/mongo_pipeline.py
+++ src/mongo_pipeline.py
@@ -8,9 +8,9 @@ from utils.config import COLLECTION, DBNAME, MG_LOG_FILE
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     filename=MG_LOG_FILE,
-    format='%(asctime)s:%(levelname)s:%(message)s',
-    level=logging.INFO
-    )
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    level=logging.INFO,
+)
 
 fake = Faker()
 
@@ -18,18 +18,16 @@ full_document = []
 
 for _ in range(500):
     user = {
-      "Uid": fake.uuid4(),
-      "firstName": fake.first_name(),
-      "lastName": fake.last_name(),
-      "occupation": fake.job(),
-      "state":  fake.state(),
-      "updated_at": datetime.now()
+        "Uid": fake.uuid4(),
+        "firstName": fake.first_name(),
+        "lastName": fake.last_name(),
+        "occupation": fake.job(),
+        "state": fake.state(),
+        "updated_at": datetime.now(),
     }
     full_document.append(user)
 
 logging.info(f"writing records to database: {DBNAME}...")
 
 COLLECTION.insert_many(full_document)
-logging.info(
-   f"writen records of {len(full_document)} to database: {DBNAME}..."
-   )
+logging.info(f"writen records of {len(full_document)} to database: {DBNAME}...")
