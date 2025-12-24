a367340b5b4a70b77ac7ee2007e8eb57a4aff37f
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-12-24 16:57:38 +0100

    updated pipelines

--- orchestration/dags/nomba_eng/include/mongo_pipeline.py
+++ orchestration/dags/nomba_eng/include/mongo_pipeline.py
@@ -11,23 +11,24 @@ logging.basicConfig(
     format="%(asctime)s:%(levelname)s:%(message)s",
     level=logging.INFO,
 )
+def write_to_mongo():
+    fake = Faker()
 
-fake = Faker()
+    full_document = []
 
-full_document = []
+    for _ in range(500):
+        user = {
+            "Uid": fake.uuid4(),
+            "firstName": fake.first_name(),
+            "lastName": fake.last_name(),
+            "occupation": fake.job(),
+            "state": fake.state(),
+            "updated_at": datetime.now(),
+        }
+        full_document.append(user)
 
-for _ in range(500):
-    user = {
-        "Uid": fake.uuid4(),
-        "firstName": fake.first_name(),
-        "lastName": fake.last_name(),
-        "occupation": fake.job(),
-        "state": fake.state(),
-        "updated_at": datetime.now(),
-    }
-    full_document.append(user)
+    logging.info(f"writing records to database: {DBNAME}...")
 
-logging.info(f"writing records to database: {DBNAME}...")
-
-COLLECTION.insert_many(full_document)
-logging.info(f"writen records of {len(full_document)} to database: {DBNAME}...")
+    COLLECTION.insert_many(full_document)
+    logging.info(f"writen records of {len(full_document)} to database: {DBNAME}...")
+    
